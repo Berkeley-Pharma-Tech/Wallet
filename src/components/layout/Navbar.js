44a7cb030b172c3f268f3a4600b1b014fea25175
Raymond <raymondtan@berkeley.edu>
2021-03-10 20:48:23 -0800

    Sticky Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,23 +1,22 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
 
+
 const AppNavbar = () => {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="dark" variant="dark" fixed="top">
             <Navbar.Brand href="/">Wallet</Navbar.Brand>
-            <Nav className="mr-auto">
+            <Nav>
                 <Nav.Link href="/home">Home</Nav.Link>
                 <Nav.Link href="/createtransaction">Create Transaction</Nav.Link>
                 <Nav.Link href="#pricing">Place Holder</Nav.Link>
             </Nav>
             <SignedOutLinks/>
             <SignedInLinks/>
-
         </Navbar>
-
     )
 }
 
